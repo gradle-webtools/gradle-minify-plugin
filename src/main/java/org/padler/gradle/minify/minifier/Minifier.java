5bf83d9e56cec49b192287aa3b585399c6c19632
patrickadler <patrick.adler@cegeka.de>
2019-04-24 09:31:09 +0200

    Use try with resources

--- src/main/java/org/padler/gradle/minify/minifier/Minifier.java
+++ src/main/java/org/padler/gradle/minify/minifier/Minifier.java
@@ -12,16 +12,13 @@ import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 public abstract class Minifier {
 
     public void minify(String srcDir, String dstDir) {
-        try {
-            Stream<Path> files = Files.walk(Paths.get(srcDir));
+        try (Stream<Path> files = Files.walk(Paths.get(srcDir))) {
             files
                     .filter(f -> !f.toString().equals(srcDir))
                     .forEach(f -> {
                         if (Files.isRegularFile(f)) {
                             Path dst = Paths.get(dstDir);
                             File dstFile = new File(dst.toString() + "/" + f.getFileName().toString());
-                            System.out.println("src: " + f.toFile());
-                            System.out.println("dst: " + dstFile);
                             minify(f.toFile(), dstFile);
                         } else if (Files.isDirectory(f, NOFOLLOW_LINKS)) {
                             minify(f.toString(), dstDir + "/" + f.getFileName().toString());
