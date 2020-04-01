8b9d8cd3ee8741170cceb1c2f6738ba53f0a5993
Patrick Adler <patrick.adler@cegeka.de>
2020-04-01 12:18:27 +0200

    Use google closure to minify js

--- src/main/java/org/padler/gradle/minify/minifier/JsMinifier.java
+++ src/main/java/org/padler/gradle/minify/minifier/JsMinifier.java
@@ -1,28 +1,46 @@
 package org.padler.gradle.minify.minifier;
 
-import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
+import com.google.common.collect.ImmutableList;
+import com.google.javascript.jscomp.*;
+import org.gradle.api.GradleException;
 
-import java.io.*;
-import java.nio.file.Files;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+import static com.google.javascript.jscomp.CompilationLevel.SIMPLE_OPTIMIZATIONS;
 
 public class JsMinifier extends Minifier {
 
+    CompilerOptions options = new CompilerOptions();
+
+    public JsMinifier() {
+        CompilationLevel.valueOf(SIMPLE_OPTIMIZATIONS.name()).setOptionsForCompilationLevel(options);
+        WarningLevel.QUIET.setOptionsForWarningLevel(options);
+    }
+
     @Override
     protected void minifyFile(File srcFile, File dstFile) throws IOException {
-        if (!Files.readString(srcFile.toPath()).isEmpty()) {
-            InputStreamReader iosR = new InputStreamReader(new FileInputStream(srcFile));
-            OutputStreamWriter iosW = new OutputStreamWriter(new FileOutputStream(dstFile));
+        com.google.javascript.jscomp.Compiler compiler = new com.google.javascript.jscomp.Compiler();
 
-            JavaScriptCompressor compressor = new JavaScriptCompressor(iosR, null);
-            compressor.compress(iosW, -1, true, false, false, false);
+        try (OutputStreamWriter iosW = new OutputStreamWriter(new FileOutputStream(dstFile))) {
 
+            List<SourceFile> externs = AbstractCommandLineRunner.getBuiltinExterns(new CompilerOptions().getEnvironment());
+            SourceFile sourceFile = SourceFile.fromFile(srcFile.getAbsolutePath());
+            Result result = compiler.compile(externs, ImmutableList.of(sourceFile), options);
+
+            if (result.success) {
+                iosW.write(compiler.toSource());
+            } else {
+                StringBuilder error = new StringBuilder();
+                for (JSError jsError : result.errors) {
+                    error.append(jsError.getSourceName()).append(":").append(jsError.getLineno()).append(" - ").append(jsError.getDescription());
+                }
+                throw new GradleException(error.toString());
+            }
             iosW.flush();
-            iosR.close();
-            iosW.close();
-        } else {
-            if (dstFile.exists())
-                Files.delete(dstFile.toPath());
-            Files.copy(srcFile.toPath(), dstFile.toPath());
         }
     }
 }
