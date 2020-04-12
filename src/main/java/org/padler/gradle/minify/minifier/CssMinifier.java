3bc677fd9ac8a7187b91b654c896a0e2ac2d9752
Patrick Adler <patrick.adler92@gmail.com>
2020-04-12 20:31:27 +0200

    Correct source map path mapping for sub directories

--- src/main/java/org/padler/gradle/minify/minifier/CssMinifier.java
+++ src/main/java/org/padler/gradle/minify/minifier/CssMinifier.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 public class CssMinifier extends Minifier {
 
     @Override
-    protected void minifyFile(File srcFile, File dstFile) {
+    protected void minifyFile(File srcFile, File dstFile, File baseDir) {
         try {
             JobDescription job = createJobDescription(srcFile);
             ExitCodeHandler exitCodeHandler = new DefaultExitCodeHandler();
--- src/main/java/org/padler/gradle/minify/minifier/LocationMapping.java
+++ src/main/java/org/padler/gradle/minify/minifier/LocationMapping.java
@@ -3,14 +3,33 @@ package org.padler.gradle.minify.minifier;
 import com.google.javascript.jscomp.SourceMap;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.util.Locale;
 
 public class LocationMapping implements SourceMap.LocationMapping {
+
+    private static String OS = System.getProperty("os.name", "unknown").toLowerCase(Locale.ROOT);
+    private String baseDir;
+
+    public LocationMapping(File baseDir) {
+        if (isWindows()) {
+            this.baseDir = baseDir.getAbsolutePath().replace("\\", "/");
+        } else {
+            this.baseDir = baseDir.getAbsolutePath();
+        }
+    }
+
     @Nullable
     @Override
     public String map(String location) {
-        if (location.contains("/"))
-            return location.substring(location.lastIndexOf("/") + 1);
+        if (location.contains(baseDir))
+            return location.substring(baseDir.length() + 1);
         else
             return null;
     }
+
+    private static boolean isWindows() {
+        return OS.contains("win");
+    }
+
 }
