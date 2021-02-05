3d964268ebbe2b7ae5a35356863c5f286f8792e9
Patrick Adler <patrick.adler@cegeka.de>
2021-02-05 10:05:01 +0100

    #8: Added "ignoreMinFiles" flag

--- src/main/kotlin/org/gradlewebtools/minify/minifier/MinifierOptions.kt
+++ src/main/kotlin/org/gradlewebtools/minify/minifier/MinifierOptions.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 open class MinifierOptions(
+        open var ignoreMinFiles: Boolean = false,
         open var createSourceMaps: Boolean = false,
         open var originalFileNames: Boolean = false,
         open var copyOriginalFile: Boolean = false
