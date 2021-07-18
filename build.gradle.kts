70effe8c4344e10b5ef4c5a470f5758be25e26c4
Patrick Adler <patrick@padler.org>
2021-07-18 10:58:45 +0200

    Release v1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.gradle-webtools.minify"
-version = "1.2.0"
+version = "1.2.1"
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
