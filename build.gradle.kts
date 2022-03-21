32550ba36b73aded7939a8436ec663b093ea02e5
Patrick Adler <patrick@padler.org>
2022-03-21 14:28:50 +0100

    Release 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
-    kotlin("plugin.serialization") version "1.5.21"
-    id("com.gradle.plugin-publish") version "0.15.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+    id("com.gradle.plugin-publish") version "0.20.0"
     id("maven-publish")
     id("java-gradle-plugin")
     id("jacoco")
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.gradle-webtools.minify"
-version = "1.3.0"
+version = "1.3.1"
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
@@ -49,9 +49,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("com.google.javascript:closure-compiler:v20210106")
-    implementation("org.padler:closure-stylesheets:1.6.2")
+    implementation("org.padler:closure-stylesheets:1.6.3")
 
     testImplementation("io.kotest:kotest-runner-junit5:4.6.1")
     testImplementation("io.kotest:kotest-assertions-core:4.6.1")
